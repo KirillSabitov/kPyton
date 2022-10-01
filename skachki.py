e3f3d01b0c4f42c9910574017322757d35d88a93
Sabitovkirill <sabitov-2009@yandex.ru>
2022-10-01 12:46:00 +0300

    skachki

---
+++ skachki.py
@@ -0,0 +1,18 @@
+from re import T
+
+
+def histori():
+    print('Вы приходите на ипподром поставить на лошадь.')
+    minstavka = input('Минимальная сумма ставки:')
+    while not minstavka.isnumeric():
+        print('Вы должны были ввести только цифры')
+        minstavka = input()
+    ncap = input('У вас есть(введите сумму):')
+    while not ncap.isnumeric():
+        print('Вы должны были ввести только цифры')
+        ncap = input()
+    while True:
+        if minstavka > ncap:
+            print('Минимальная ставка не может быть больше вашего изначального капитала.')
+            return True
+print(histori())
