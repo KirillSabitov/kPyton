18a44dc2c83af1b5fdf2cab45de00ad8d59b0a74
KirillSabitov <Sabitov-2009@yandex.ru>
2022-10-22 16:02:45 +0300

    m

--- 21.py
+++ 21.py
@@ -1,4 +1,4 @@
-import random,time
+import random
 
 def displayboard():
     board = ['''
@@ -47,11 +47,9 @@ def pravila():
     while True:
         otvet = input().lower()
         if  (otvet =='да') or (otvet =='д') or (otvet =='yes') or (otvet =='y'):
-            time.sleep(2)
             print('''Вы кидаете игральные кости, пока не наберут общую сумму:21 или приблизительно 21 очко.
 Если игрок набрал больше 21 очка, то все баллы аннулируются и игрок проигрывает. 
 Вы можете остановиться и не рисковать, передав ход компьютеру, он будет пытаться перебить ваш счет.''')
-            time.sleep(2)
             a = input('Нажмите enter для продолжения')
             return True
         elif (otvet =='нет') or (otvet =='н') or (otvet =='no') or (otvet =='n'):
@@ -70,7 +68,6 @@ def getrandomcislo():
     return kub
 
 
-
 def playAgen():
     print('Хотите сыграть еще раз?')
     while True:
@@ -87,43 +84,58 @@ def playAgen():
 Введите "да" для продолжения и "нет" для завершения.''')
 
 
-
-def display(board,kubik1,kubik2,balg,balk,gamer):
+def display(gamer,board,kubik1,kubik2,balk,balg):
     print(gamer)
+    print()
     print(board[kubik1])
     print(board[kubik2])
     print()
     print('''Количество очков:
-    компьютер:'''+balk+'''
-    Вы:'''+balg)
-
-def hod():
-    print('Нажмите б чтобы бросить ')
-    while True:
-        otvet = input().lower().startswith()
-        if otvet == 'б':
-            return True
-        else:
-            print('''Я вас не понял.
-Введите ответ еще раз.''')
-
-
-def vbros():
-    print('Вы хотите передать ход?')
-    otvet = input().lower()
-    if  (otvet =='да') or (otvet =='д') or (otvet =='yes') or (otvet =='y'):
-        return True  
-
-    elif (otvet =='нет') or (otvet =='н') or (otvet =='no') or (otvet =='n'):
-        return False
-
-
-#def proverka(balg,balk):
-pravila()
-
-
+    компьютер:'''+str(balk)+'''
+    Вы:'''+str(balg))
     
 
 
 
+
+pravila()
+ktobrosaet = 'Человек'
+kub = displayboard()
+balk = 0
+balg = 0
+game = True
+gamer = True
+computer = True
+
+while game:
+    while gamer:
+        kubik1,kubik2 = getrandomcislo()
+        balg = balg + kubik1 + kubik2
+        display(ktobrosaet,kub,kubik1,kubik2,balk,balg)
+        if balg > 21:
+            print('Вы проиграли!')
+            game = False
+            gamer = False
+            computer = False
+        if game and not (input('(б)росаем еще или (п)ередаем ход?').upper()=='Б'):
+            gamer = False
+    while computer:
+        kubik1,kubik2 = getrandomcislo()
+        balk = balk + kubik1 + kubik2
+        display(ktobrosaet,kub,kubik1,kubik2,balk,balg)
+        if balk > 21:
+            print('Вы выиграли!')
+            game = False
+            computer = False
+        elif balk > balg:
+            print('Вы проиграли!')
+            game = False
+            computer = False
+        elif balk == balg:
+            print('Ничья!')
+            game = False
+            computer = False
+        a = input('Нажмите enter для продолжения')
+playAgen()
+        
 
